8c1726f2b98d90d8e26b811c26ea1d68fc3b1273
Juan <j.tejeda46@yahoo.com>
2023-11-21 13:58:58 -0600

    Functional Instantiation with object create

--- js/objectCreation.js
+++ js/objectCreation.js
@@ -1,12 +1,12 @@
 // Simple JS Object Creation
 
-let knight = {
+let knightObj = {
 	level: 50,
 	hitpoints: 35,
 	weapon: 'Sword'
 }
 
-console.log(knight)
+console.log(knightObj)
 
 // Complex JS Object Creation
 let weapon = {
@@ -32,7 +32,7 @@ console.log(knightTwo.phrases[1])
 // Objects with functions
 	// Objects also have a built in 'this' keyword
 
-let archer = {
+let archerObj = {
 	level: 20,
 	weapon: weapon.range,
 	stats: {
@@ -45,4 +45,42 @@ let archer = {
 	}
 }
 
-archer.retalialte()
+archerObj.retalialte()
+
+console.clear()
+
+//////////////////////////////
+// Functional Instantiation //
+//////////////////////////////
+
+const defenderMethods = {
+	takeDamage: function(damage){
+		console.log(`${this.classType} has Taken ${damage} damage worth of hitpoints!`);
+		this.hitpoints -= damage;
+		console.log(`Hitpoints now at: ${this.hitpoints}`);
+	},
+
+	healHitpoints: function(heal){
+		console.log(`${this.classType} has healed ${heal} hitpoints!`);
+		this.hitpoints += heal;
+		console.log(`Hitpoints now at: ${this.hitpoints}`)
+	} 
+}
+
+function Defender(classType, level, hitpoints){
+	let defender = Object.create(defenderMethods);
+	defender.classType = classType;
+	defender.level = level;
+	defender.hitpoints = hitpoints;
+
+	return defender
+
+}
+
+const knight = Defender('Knight', 20, 100)
+const archer = Defender('Archer', 15, 100)
+const mage = Defender('Mage', 20, 100)
+
+console.log(knight.takeDamage(20))
+
+console.log(knight.healHitpoints(20))
